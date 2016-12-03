11c8d343ff559ac73a11f6d1d1b2ce09c66be3c0
AhnYeonsun <yeonsun0517@gmail.com>
2016-12-03 15:56:34 +0900

    Use database BingoDB
    add
    SELECT id, question, answer
    FROM game(table) in BingoDB(database)

--- Network_termP/src/ThreeGo/connectDatabase.java
+++ Network_termP/src/ThreeGo/connectDatabase.java
@@ -4,7 +4,11 @@ import java.sql.*;
 import javax.net.ssl.SSLContext;
 import javax.*;
 public class connectDatabase {
-	public static void main(String[] args){
+	public static int id;
+	public static String question;
+	public static String answer;
+	
+	public static Connection makeConnection() {
 		String url = "jdbc:mysql://127.0.0.1:3306/game";
 		String id = "root";
 		String password = "12345";
@@ -22,6 +26,22 @@ public class connectDatabase {
 			System.out.println("My-SQL 立加 己傍!!");
 		}catch(SQLException e) {
 			System.out.println("My-SQL 立加 角菩");
+		}
+		return con;
+	}
+	public static void main(String[] args){
+		try{
+			Connection con = makeConnection();
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT* FROM game");
+			
+			while(rs.next()){
+				id = rs.getInt("ID");
+				question = rs.getString("QUENSTION");
+				answer = rs.getString("ANSWER");
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
 		}
 	}
 }
