228867c13a8fb99427d56e441eda78bd618312d8
AhnYeonsun <yeonsun0517@gmail.com>
2016-12-06 21:52:00 +0900

    Bingocheck_modify

--- Network_termP/src/ThreeGo/isBingo.java
+++ Network_termP/src/ThreeGo/isBingo.java
@@ -6,41 +6,50 @@ import javax.swing.JOptionPane;
 
 public class isBingo {
 
-
-	public isBingo(int[] board){
-		int i=0,j=0;
+	static boolean[] f = new boolean[12];
+	static int cnt = 0;
+	
+	private void yesbingo(int i){
+		f[i] = true;
+		cnt = 0;
+		for (boolean myBool : f){
+			if(myBool){
+				cnt++;
+			}
+		}
 		
+		if(cnt == 3){
+			JOptionPane.showMessageDialog(null, "You win", "Bingocheck message",JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
+	public isBingo(int[] board){
+		
+
 		if(board[1]==1 && board[2]==1 && board[3]==1 && board[4]==1 && board[5]==1) //horizontal
-			j++;
+			yesbingo(0);
 		if(board[6]==1 && board[7]==1 && board[8]==1 && board[9]==1 && board[10]==1)
-			j++;
+			yesbingo(1);
 		if(board[11]==1 && board[12]==1 && board[13]==1 && board[14]==1 && board[15]==1)
-			j++;
+			yesbingo(2);
 		if(board[16]==1 && board[17]==1 && board[18]==1 && board[19]==1 && board[20]==1)
-			j++;
+			yesbingo(3);
 		if(board[21]==1 && board[22]==1 && board[23]==1 && board[24]==1 && board[25]==1)
-			j++;
+			yesbingo(4);
 
 		if(board[1]==1 && board[6]==1 && board[11]==1 && board[16]==1 && board[21]==1) //vertical
-			j++;
+			yesbingo(5);
 		if(board[2]==1 && board[7]==1 && board[12]==1 && board[17]==1 && board[12]==1)
-			j++;
+			yesbingo(6);
 		if(board[3]==1 && board[8]==1 && board[13]==1 && board[18]==1 && board[23]==1)
-			j++;
+			yesbingo(7);
 		if(board[4]==1 && board[9]==1 && board[14]==1 && board[19]==1 && board[24]==1)
-			j++;
+			yesbingo(8);
 		if(board[5]==1 && board[10]==1 && board[15]==1 && board[20]==1 && board[25]==1)
-			j++;
+			yesbingo(9);
 
 		if(board[1]==1 && board[7]==1 && board[13]==1 && board[19]==1 && board[25]==1) //diagonal
-			j++;
+			yesbingo(10);
 		if(board[5]==1 && board[9]==1 && board[13]==1 && board[17]==1 && board[21]==1)
-			j++;
-
-		if(j==3)
-	       {
-	           JOptionPane.showMessageDialog(null, "U win", "U win the match",JOptionPane.INFORMATION_MESSAGE);
-	           PrintWriter out;
-	       }
+			yesbingo(11);
 	}
 }
