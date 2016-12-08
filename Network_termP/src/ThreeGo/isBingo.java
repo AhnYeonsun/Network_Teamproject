f852a124746ac75ad0d643f7297b8f7d33b2d0e0
AhnYeonsun <yeonsun0517@gmail.com>
2016-12-08 22:30:17 +0900

    n bingo float msg

--- Network_termP/src/ThreeGo/isBingo.java
+++ Network_termP/src/ThreeGo/isBingo.java
@@ -48,32 +48,56 @@ public class isBingo {
 
    //각 대각선과 가로 세로 를 세어서 빙고의 수를 세어준다.
    public isBingo(int[] board){
-
-      if(board[1]==1 && board[2]==1 && board[3]==1 && board[4]==1 && board[5]==1) //horizontal
+	  //horizontal
+      if(board[1]==1 && board[2]==1 && board[3]==1 && board[4]==1 && board[5]==1){
          yesbingo(0);
-      if(board[6]==1 && board[7]==1 && board[8]==1 && board[9]==1 && board[10]==1)
+         JOptionPane.showMessageDialog(null, String.valueOf(cnt) +" Bingo", "Bingocheck message",JOptionPane.INFORMATION_MESSAGE);
+      }
+      if(board[6]==1 && board[7]==1 && board[8]==1 && board[9]==1 && board[10]==1){
          yesbingo(1);
-      if(board[11]==1 && board[12]==1 && board[13]==1 && board[14]==1 && board[15]==1)
+         JOptionPane.showMessageDialog(null, String.valueOf(cnt) +" Bingo", "Bingocheck message",JOptionPane.INFORMATION_MESSAGE);
+      }
+      if(board[11]==1 && board[12]==1 && board[13]==1 && board[14]==1 && board[15]==1){
          yesbingo(2);
-      if(board[16]==1 && board[17]==1 && board[18]==1 && board[19]==1 && board[20]==1)
+         JOptionPane.showMessageDialog(null, String.valueOf(cnt) +" Bingo", "Bingocheck message",JOptionPane.INFORMATION_MESSAGE);
+      }
+      if(board[16]==1 && board[17]==1 && board[18]==1 && board[19]==1 && board[20]==1){
          yesbingo(3);
-      if(board[21]==1 && board[22]==1 && board[23]==1 && board[24]==1 && board[25]==1)
+         JOptionPane.showMessageDialog(null, String.valueOf(cnt) +" Bingo", "Bingocheck message",JOptionPane.INFORMATION_MESSAGE);
+      }
+      if(board[21]==1 && board[22]==1 && board[23]==1 && board[24]==1 && board[25]==1){
          yesbingo(4);
-
-      if(board[1]==1 && board[6]==1 && board[11]==1 && board[16]==1 && board[21]==1) //vertical
+         JOptionPane.showMessageDialog(null, String.valueOf(cnt) +" Bingo", "Bingocheck message",JOptionPane.INFORMATION_MESSAGE);
+      }
+      //vertical
+      if(board[1]==1 && board[6]==1 && board[11]==1 && board[16]==1 && board[21]==1){
          yesbingo(5);
-      if(board[2]==1 && board[7]==1 && board[12]==1 && board[17]==1 && board[12]==1)
+         JOptionPane.showMessageDialog(null, String.valueOf(cnt) +" Bingo", "Bingocheck message",JOptionPane.INFORMATION_MESSAGE);
+      }
+      if(board[2]==1 && board[7]==1 && board[12]==1 && board[17]==1 && board[12]==1){
          yesbingo(6);
-      if(board[3]==1 && board[8]==1 && board[13]==1 && board[18]==1 && board[23]==1)
+         JOptionPane.showMessageDialog(null, String.valueOf(cnt) +" Bingo", "Bingocheck message",JOptionPane.INFORMATION_MESSAGE);
+      }
+      if(board[3]==1 && board[8]==1 && board[13]==1 && board[18]==1 && board[23]==1){
          yesbingo(7);
-      if(board[4]==1 && board[9]==1 && board[14]==1 && board[19]==1 && board[24]==1)
+         JOptionPane.showMessageDialog(null, String.valueOf(cnt) +" Bingo", "Bingocheck message",JOptionPane.INFORMATION_MESSAGE);
+      }
+      if(board[4]==1 && board[9]==1 && board[14]==1 && board[19]==1 && board[24]==1){
          yesbingo(8);
-      if(board[5]==1 && board[10]==1 && board[15]==1 && board[20]==1 && board[25]==1)
+         JOptionPane.showMessageDialog(null, String.valueOf(cnt) +" Bingo", "Bingocheck message",JOptionPane.INFORMATION_MESSAGE);
+      }
+      if(board[5]==1 && board[10]==1 && board[15]==1 && board[20]==1 && board[25]==1){
          yesbingo(9);
-
-      if(board[1]==1 && board[7]==1 && board[13]==1 && board[19]==1 && board[25]==1) //diagonal
+         JOptionPane.showMessageDialog(null, String.valueOf(cnt) +" Bingo", "Bingocheck message",JOptionPane.INFORMATION_MESSAGE);
+      }
+      //diagonal
+      if(board[1]==1 && board[7]==1 && board[13]==1 && board[19]==1 && board[25]==1){
          yesbingo(10);
-      if(board[5]==1 && board[9]==1 && board[13]==1 && board[17]==1 && board[21]==1)
+         JOptionPane.showMessageDialog(null, String.valueOf(cnt) +" Bingo", "Bingocheck message",JOptionPane.INFORMATION_MESSAGE);
+      }
+      if(board[5]==1 && board[9]==1 && board[13]==1 && board[17]==1 && board[21]==1){
          yesbingo(11);
+         JOptionPane.showMessageDialog(null, String.valueOf(cnt) +" Bingo", "Bingocheck message",JOptionPane.INFORMATION_MESSAGE);
+      }
    }
 }
